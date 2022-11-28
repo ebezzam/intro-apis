fc3faacb034104514b769db674750853c2dbf228
Eric Bezzam <ebezzam@gmail.com>
2022-11-28 17:19:02 +0100

    Add hugging face example.

---
+++ hugging_face.py
@@ -0,0 +1,15 @@
+from keys import HUGGING_FACE
+import requests
+
+API_URL = "https://api-inference.huggingface.co/models/distilbert-base-uncased-finetuned-sst-2-english"
+headers = {"Authorization": "Bearer " + HUGGING_FACE}
+
+def query(payload):
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.json()
+	
+output = query({
+	"inputs": "I like you. I love you",
+})
+
+print(output)
